8002bad5e3e2552768838a247aee51db0ff44178
William Rudenmalm <me@whn.se>
2018-01-15 11:50:47 +0100

    Working ls for gstorage

--- src/sftpserver/stub_sftp.py
+++ src/sftpserver/stub_sftp.py
@@ -22,18 +22,25 @@ A stub SFTP server for loopback SFTP testing.
 
 import os
 from paramiko import ServerInterface, SFTPServerInterface, SFTPServer, SFTPAttributes, \
-    SFTPHandle, SFTP_OK, AUTH_SUCCESSFUL, OPEN_SUCCEEDED
+    SFTPHandle, SFTP_OK, AUTH_SUCCESSFUL, OPEN_SUCCEEDED, SFTP_PERMISSION_DENIED, SFTP_NO_SUCH_FILE
+from google.cloud import storage
+from io import BytesIO
 
+PROJECT_ID = os.environ["GCP_PROJECT_ID"]
+BUCKET = os.environ["GCP_STORAGE_BUCKET"]
+
+def get_storage_client():
+    return storage.Client(project=PROJECT_ID)
 
 class StubServer (ServerInterface):
     def check_auth_password(self, username, password):
         # all are allowed
         return AUTH_SUCCESSFUL
-        
+
     def check_auth_publickey(self, username, key):
         # all are allowed
         return AUTH_SUCCESSFUL
-        
+
     def check_channel_request(self, kind, chanid):
         return OPEN_SUCCEEDED
 
@@ -50,165 +57,106 @@ class StubSFTPHandle (SFTPHandle):
             return SFTPServer.convert_errno(e.errno)
 
     def chattr(self, attr):
-        # python doesn't have equivalents to fchown or fchmod, so we have to
-        # use the stored filename
-        try:
-            SFTPServer.set_file_attr(self.filename, attr)
-            return SFTP_OK
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
+        return SFTP_PERMISSION_DENIED
 
+def blob_to_stat(blob):
+    attr = SFTPAttributes()
+    attr.filename = blob.name
+    attr.st_size = blob.size
+    attr.st_mode = 0o0100444
+    attr.st_uid = 1000
+    attr.st_gid = 1000
+    attr.st_ctime = blob.time_created
+    attr.st_mtime = 1
+    attr.st_atime = 1
+    print(attr)
+    return attr
 
 class StubSFTPServer (SFTPServerInterface):
     # assume current folder is a fine root
     # (the tests always create and eventualy delete a subfolder, so there shouldn't be any mess)
     ROOT = os.getcwd()
-        
+
+    def __init__(self, *args, **kwargs):
+        self.client = get_storage_client()
+        self.bucket = self.client.get_bucket(BUCKET)
+        if self.bucket is None:
+            raise RuntimeError("Missing bucket")
+        super().__init__(*args, **kwargs)
+
+    def get_file(fname):
+        print(fname.strip("/"))
+        return self.bucket.get_blob(fname.strip("/"))
+
     def _realpath(self, path):
         return self.ROOT + self.canonicalize(path)
 
+
     def list_folder(self, path):
-        path = self._realpath(path)
-        try:
-            out = [ ]
-            flist = os.listdir(path)
-            for fname in flist:
-                attr = SFTPAttributes.from_stat(os.stat(os.path.join(path, fname)))
-                attr.filename = fname
-                out.append(attr)
-            return out
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
+        res = self.bucket.list_blobs(prefix=path if path != "/" else None, max_results=1000, delimiter="/")
+        return [blob_to_stat(blob) for blob in res]
+
 
     def stat(self, path):
-        path = self._realpath(path)
+        print("stat {}".format(path))
         try:
-            return SFTPAttributes.from_stat(os.stat(path))
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
+            blob = self.get_file(path)
+            return blob_to_stat(blob)
+        except ex:
+            print(ex)
 
     def lstat(self, path):
-        path = self._realpath(path)
+        print("stat {}".format(path))
         try:
-            return SFTPAttributes.from_stat(os.lstat(path))
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
+            blob = self.get_file(path)
+            print(blob)
+            return blob_to_stat(blob)
+        except ex:
+            print(ex)
 
     def open(self, path, flags, attr):
-        path = self._realpath(path)
-        try:
-            binary_flag = getattr(os, 'O_BINARY',  0)
-            flags |= binary_flag
-            mode = getattr(attr, 'st_mode', None)
-            if mode is not None:
-                fd = os.open(path, flags, mode)
-            else:
-                # os.open() defaults to 0777 which is
-                # an odd default mode for files
-                fd = os.open(path, flags, 0o666)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
-        if (flags & os.O_CREAT) and (attr is not None):
-            attr._flags &= ~attr.FLAG_PERMISSIONS
-            SFTPServer.set_file_attr(path, attr)
-        if flags & os.O_WRONLY:
-            if flags & os.O_APPEND:
-                fstr = 'ab'
-            else:
-                fstr = 'wb'
-        elif flags & os.O_RDWR:
-            if flags & os.O_APPEND:
-                fstr = 'a+b'
-            else:
-                fstr = 'r+b'
-        else:
-            # O_RDONLY (== 0)
-            fstr = 'rb'
-        try:
-            f = os.fdopen(fd, fstr)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
+        # Writing is not supported
+        print("rheep", path)
+        print(flags)
+        print(attr)
+        bfr = BytesIO()
+
+        blob = self.get_file(path.strip("/"))
+        print("wheep")
+        if blob is None:
+            return SFTP_NO_SUCH_FILE
+
+        blob.download_to_file(bfr)
+
+        bfr.seek(0)
+
         fobj = StubSFTPHandle(flags)
         fobj.filename = path
-        fobj.readfile = f
-        fobj.writefile = f
+        fobj.readfile = bfr
+        fobj.writefile = None
         return fobj
 
     def remove(self, path):
-        path = self._realpath(path)
-        try:
-            os.remove(path)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
+        # TODO: this needs handling
         return SFTP_OK
 
     def rename(self, oldpath, newpath):
-        oldpath = self._realpath(oldpath)
-        newpath = self._realpath(newpath)
-        try:
-            os.rename(oldpath, newpath)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
-        return SFTP_OK
+        return SFTP_PERMISSION_DENIED
 
     def mkdir(self, path, attr):
-        path = self._realpath(path)
-        try:
-            os.mkdir(path)
-            if attr is not None:
-                SFTPServer.set_file_attr(path, attr)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
-        return SFTP_OK
+        return SFTP_PERMISSION_DENIED
 
     def rmdir(self, path):
-        path = self._realpath(path)
-        try:
-            os.rmdir(path)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
-        return SFTP_OK
+        # May not remove directories
+        return SFTP_PERMISSION_DENIED
 
     def chattr(self, path, attr):
-        path = self._realpath(path)
-        try:
-            SFTPServer.set_file_attr(path, attr)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
-        return SFTP_OK
+        # Not allowed to change permissions
+        return SFTP_PERMISSION_DENIED
 
     def symlink(self, target_path, path):
-        path = self._realpath(path)
-        if (len(target_path) > 0) and (target_path[0] == '/'):
-            # absolute symlink
-            target_path = os.path.join(self.ROOT, target_path[1:])
-            if target_path[:2] == '//':
-                # bug in os.path.join
-                target_path = target_path[1:]
-        else:
-            # compute relative to path
-            abspath = os.path.join(os.path.dirname(path), target_path)
-            if abspath[:len(self.ROOT)] != self.ROOT:
-                # this symlink isn't going to work anyway -- just break it immediately
-                target_path = '<error>'
-        try:
-            os.symlink(target_path, path)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
-        return SFTP_OK
+        # Can't create symlinks
+        return SFTP_PERMISSION_DENIED
 
     def readlink(self, path):
-        path = self._realpath(path)
-        try:
-            symlink = os.readlink(path)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
-        # if it's absolute, remove the root
-        if os.path.isabs(symlink):
-            if symlink[:len(self.ROOT)] == self.ROOT:
-                symlink = symlink[len(self.ROOT):]
-                if (len(symlink) == 0) or (symlink[0] != '/'):
-                    symlink = '/' + symlink
-            else:
-                symlink = '<error>'
-        return symlink
+        return SFTP_PERMISSIONED_DENIED
